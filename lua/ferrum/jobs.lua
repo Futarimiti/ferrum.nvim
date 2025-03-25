95749423ad14893ebf02cc9929e45fbcaad5f844
Carman Fu <futarimiti@163.com>
2025-03-25 19:19:57 +0000

    wip: purge deprecated functions

--- lua/ferrum/jobs.lua
+++ lua/ferrum/jobs.lua
@@ -1,14 +1,9 @@
--- A huge global variable storing info about each job.
-
--- Record for each job
----@type Jobs
-local jobs = {}
+-- Job utilities
 
 local Jobs = {}
 
 -- List all active terminal jobs that are connected to a buffer.
----@return table<integer,{cmd:string,buf:integer}> table mapping job id to info
----@deprecated Leave it for now
+---@return Jobs
 Jobs.all = function()
   return vim.iter(vim.api.nvim_list_chans()):fold({}, function(acc, chaninfo)
     if
@@ -24,61 +19,7 @@ Jobs.all = function()
   end)
 end
 
---- DEPRECATED
-
--- Allow any arbitrary mutation on jobs db.
--- If the given function returns anything, return it.
----@generic T
----@param f fun(_:Jobs): T
----@return T
----@deprecated
-Jobs.mut = function(f) return f(jobs) end
-
----@param job integer
----@return JobRecord?
----@deprecated
-Jobs.get = function(job) return Jobs.all()[job] end
-
----@param job integer
----@param record JobRecord
----@deprecated
-Jobs.set = function(job, record)
-  Jobs.mut(function(jobs_)
-    jobs_[job] = record
-    return nil
-  end)
-end
-
----@param job integer
----@deprecated
-Jobs.del = function(job)
-  ---@diagnostic disable-next-line: param-type-mismatch
-  Jobs.set(job, nil)
-end
-
--- Make {client} a client of {job}
----@param job integer
----@param client integer
----@deprecated
-Jobs.link = function(job, client) table.insert(jobs[job].clients, client) end
-
--- Make {client} no longer a client of {job}
----@param job integer
----@param client integer
----@deprecated
-Jobs.unlink = function(job, client)
-  local clients = jobs[job].clients
-  jobs[job].clients = vim
-    .iter(clients)
-    :filter(function(c) return c ~= client end)
-    :totable()
-end
-
 return Jobs
 
----@class JobRecord
----@field clients integer[] client buffers
----@field repl integer REPL buffer
----@field cmd string[] REPL command
-
----@alias Jobs table<integer,JobRecord>
+---@alias JobInfo {cmd:string,buf:integer}
+---@alias Jobs table<integer,JobInfo>
