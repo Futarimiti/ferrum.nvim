e1e11a0ceb5307b3563de2afb1c09f6d80020114
Carman Fu <futarimiti@163.com>
2025-03-25 19:18:45 +0000

    wip: use au TermClose only

--- lua/ferrum/autocmds/buflocal.lua
+++ lua/ferrum/autocmds/buflocal.lua
@@ -4,20 +4,22 @@ local M = {}
 
 local group = vim.api.nvim_create_augroup('ferrum-repl', {})
 
+-- As the REPL session finishes, free the client buffer (if it's still valid).
 ---@param o ferrum.buflocal.commands.setup.args
 M.setup = function(o)
-  vim.api.nvim_create_autocmd({ 'BufWipeout', 'TermClose' }, {
+  local cmd = vim.fn.join(o.cmd)
+  vim.api.nvim_create_autocmd({ 'TermClose' }, {
     buffer = o.repl,
     group = group,
     once = true,
     desc = ('Clear buf #%d local cmds on job #%d (!%s) finish'):format(
       o.client,
       o.job,
-      o.cmd
+      cmd
     ),
     callback = function()
       vim.notify(
-        ('Finished: !%s (job %d)'):format(vim.fn.join(o.cmd, ' '), o.job),
+        ('Finished: !%s (job %d)'):format(cmd, o.job),
         vim.log.levels.INFO
       )
       require('ferrum.buffer').free(o.client, false, true)
