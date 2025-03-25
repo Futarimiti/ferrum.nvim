ef3de95ce2fa53055e2c2dee65ddee92a4a5a522
Carman Fu <futarimiti@163.com>
2025-03-25 18:51:32 +0000

    wip: drop jobs dep for buffer.lua

--- lua/ferrum/buffer.lua
+++ lua/ferrum/buffer.lua
@@ -1,13 +1,12 @@
-local Jobs = require 'ferrum.jobs'
 local Commands = { Buflocal = require 'ferrum.commands.buflocal' }
 
 local Buffer = {}
 
 -- If the given buffer is a client, unlink it and clear any buflocal commands
 ---@param buf integer client buffer
----@param notify? boolean say something
+---@param _? boolean no effect; for backwards compatibility
 ---@param tolerate? boolean tolerate invalid buffer?
-Buffer.free = function(buf, notify, tolerate)
+Buffer.free = function(buf, _, tolerate)
   if not vim.api.nvim_buf_is_valid(buf) then
     if tolerate then
       return
@@ -19,25 +18,8 @@ Buffer.free = function(buf, notify, tolerate)
   -- 1. cleanup buflocal commands
   Commands.Buflocal.cleanup(buf)
 
-  local old_job = vim.b[buf].ferrum_job
-  if old_job ~= nil then
-    local maybe_job_record = Jobs.get(old_job)
-    if maybe_job_record ~= nil then
-      -- 2. unlink jobs in job DB
-      Jobs.unlink(old_job, buf)
-      if notify then
-        vim.notify(
-          ('Unlinked job #%d (!%s)'):format(
-            old_job,
-            vim.fn.join(maybe_job_record.cmd, ' ')
-          ),
-          vim.log.levels.INFO
-        )
-      end
-    end
-    -- 3. set b:ferrum_job to nil
-    vim.b[buf].ferrum_job = nil
-  end
+  -- 2. set b:ferrum_job to nil
+  vim.b[buf].ferrum_job = nil
 end
 
 -- Like `vim.bo[buf].channel` but with explicit nil returns.
